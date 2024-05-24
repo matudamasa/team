2f269f719d78ded58ade1818bd8417141191b792
yoshiki <94036154+yoshikiLC1C@users.noreply.github.com>
2024-05-24 16:26:00 +0900

    プレイヤーの移動

--- Assets/ControlPlayer.cs
+++ Assets/ControlPlayer.cs
@@ -9,14 +9,17 @@ public class ControlPlayer : MonoBehaviour
 
     private const float RotateSpeed = 720f;
 
-    private Transform _player;
-
     private Rigidbody rb;
+
+    //[SerializeField]
+    private GameObject camera;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        camera = GameObject.Find("/Camera");
 
     }
 
@@ -41,18 +44,18 @@ public class ControlPlayer : MonoBehaviour
     private void UpdatePosition(Vector3 direction)
     {
         // カメラの方向から、X-Z平面の単位ベクトルを取得
-        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 cameraForward = Vector3.Scale(camera.transform.forward, new Vector3(1, 0, 1)).normalized;
 
         // 方向キーの入力値とカメラの向きから、移動方向を決定
-        Vector3 moveForward = cameraForward * direction.z + Camera.main.transform.right * direction.x;
+        Vector3 moveForward = cameraForward * direction.z + camera.transform.right * direction.x;
         // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す
         rb.velocity = moveForward * Speed + new Vector3(0, rb.velocity.y, 0);
         
-        // キー入力により移動方向が決まっている場合には、キャラクターの向きを進行方向に合わせる
+        // キー入a力により移動方向が決まっている場合には、キャラクターの向きを進行方向に合わせる
         if (moveForward != Vector3.zero)
         {
-            Quaternion from = _player.rotation;
-            _player.rotation = Quaternion.RotateTowards(from, Quaternion.LookRotation(moveForward),
+            Quaternion from = transform.rotation;
+            transform.rotation = Quaternion.RotateTowards(from, Quaternion.LookRotation(moveForward),
                 RotateSpeed * Time.deltaTime);
         }
     }
